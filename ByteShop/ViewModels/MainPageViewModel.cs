015a3bf56cfcca6a2ec09aefcfebce2bf0e03822
Dmitry Ignatiev <lovesan.ru at gmail.com>
2013-04-19 01:03:30 +0400

    Initial commit

---
+++ ByteShop/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ByteShop.ViewModels
+{
+    public class MainPageViewModel : ViewModelBase
+    {
+        public override string Title
+        {
+            get { return "Main page"; }
+        }
+    }
+}
---
+++ ByteShop/ViewModels/ViewModelBase.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ByteShop.ViewModels
+{
+    public class ViewModelBase
+    {
+        public virtual string Title {
+            get { return ""; }
+        }
+    }
+}
